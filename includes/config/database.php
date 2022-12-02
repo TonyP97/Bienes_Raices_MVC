6b9db0644bfeaaa3933e2b02c8d848ed2559ae61
unknown <agustinpincheira97@gmail.com>
2022-12-01 22:59:27 -0300

    ...deploy

--- includes/config/database.php
+++ includes/config/database.php
@@ -1,7 +1,7 @@
 <?php
 
 function conectarDB() : mysqli {
-    $db = new mysqli($_ENV['DB_HOST'], $_ENV['DB_USER'], $_ENV['DB_PASSWORD'], $_ENV['DB_NAME']);
+    $db = new mysqli($_ENV['DB_HOST'], $_ENV['DB_USER'], $_ENV['DB_PASSWORD'], $_ENV['DB_NAME'], $_ENV['DB_PORT']);
 
 
     if(!$db) {
